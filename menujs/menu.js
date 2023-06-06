3f3867bda3b2d378842aaaff4738026ee5f104f3
wilfried kamdoum <kamdoumwilfried8@gmail.com>
2023-06-06 04:12:38 +0100

    V2 thumbnail phone-responsiveness menu page

--- menujs/menu.js
+++ menujs/menu.js
@@ -1,7 +1,21 @@
-const productNotification = document.getElementById('basket-item-count');
-const basketDiv = document.getElementById('BasketPart');
-productElements = JSON.parse(localStorage.getItem('productBag'));
-productNotification.innerHTML=`${productElements.length}` || "0";
+function updateBasket(){
+    const productNotification = document.getElementById('basket-item-count');
+    const basketDiv = document.getElementById('BasketPart');
+    productElements = JSON.parse(localStorage.getItem('productBag'));
+    productNotification.innerHTML=`${productElements.length}` || "0";
+}
+function updateBasket2(){
+    const productNotification = document.getElementById('basket-item-count2');
+    const basketDiv = document.getElementById('BasketPart');
+    productElements = JSON.parse(localStorage.getItem('productBag'));
+    productNotification.innerHTML=`${productElements.length}` || "0";
+}
+updateBasket();
+updateBasket2();
+let counter=0;
+const products  = JSON.parse(localStorage.getItem('Products'));
+const productDetailDiv = document.getElementById('productDetailDiv');
+
 // 
 //     alert("hello");
 // }
